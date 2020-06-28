0b78318942796624f6af4376438dd75a56e200dc
Marta <marta.nunes.abreu@gmail.com>
2020-06-28 15:16:17 +0100

    cleaned up

--- transactions.r
+++ transactions.r
@@ -2,24 +2,21 @@ library(tidyverse)
 library(tidyr)
 library(magrittr)
 library(ggplot2)
-
 #### Import data and tidy up#####
-data <- read_csv("data.csv")
-df <- select(data, "Transaction Date", "Debit Amount", "Credit Amount", "Transaction Description") %>%
-  separate(. , "Transaction Date", c("day", "month", "year"), sep = "/") 
-colnames(df) <- c("day", "month", "year", "out", "incoming", "desc")
-df[is.na(df)] <- 0
+df <- read_csv("data.csv") %>%
+  select(. , "Transaction Date", "Debit Amount", "Credit Amount", "Transaction Description") %>%
+  separate(. , "Transaction Date", c("day", "month", "year"), sep = "/") %>%
+  rename(out = "Debit Amount",
+        incoming = "Credit Amount",
+        description = "Transaction Description") %>%
+  mutate(out = replace_na(out, 0),
+         incoming = replace_na(incoming, 0)) %>%
+  mutate(total = -out+incoming)
+
 
 # Expenditure in each month
-df$month <- as.numeric(df$month)
-ggplot(data = df) +
-  geom_point(mapping = aes(x = day, y = out, color = month))
-
-
-df <- mutate(df, 
-             added_up = -out+incoming) 
-
-invested <- filter(df, desc=="AJ BELL" | desc=="M NUNES DE ABREU") %>%
+# df$month <- as.numeric(df$month)
+invested <- filter(df, description=="AJ BELL" | description=="M NUNES DE ABREU") %>%
   group_by(month) %>%
   summarise(
     invested = sum(out)
@@ -30,13 +27,10 @@ by_month <- group_by(df, month) %>%
     total_out = sum(added_up),
     total_in = sum(incoming)) %>%
   full_join(invested)
-  
+
 ggplot(data = by_month) +
-  geom_line(mapping = aes(x = month, y = total_out), color = "red") +
-  geom_line(mapping = aes(x = month, y = invested), color = "blue") +
+  geom_point(mapping = aes(x = month, y = total_out), color = "red") +
+  geom_point(mapping = aes(x = month, y = invested), color = "blue") +
   ggtitle("Expenditure across months") +
   xlab("Months") +
   ylab("Expenses")
-
-
-
